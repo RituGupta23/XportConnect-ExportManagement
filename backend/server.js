b8a5c247c2f329deb3a999d2accdea3716f9632c
RituGupta23 <ritugupta232004@gmail.com>
2025-04-04 19:13:12 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // For parsing JSON data
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
