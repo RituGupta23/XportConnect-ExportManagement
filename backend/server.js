c52b7675b899b02ad6c9b14b546e70f7b3abd6bd
RituGupta23 <ritugupta232004@gmail.com>
2025-04-10 21:37:33 +0530

    Authorization Setup

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // For parsing JSON data
+
+app.use('/api/auth', authRoutes);
 
 connectDB();
 
